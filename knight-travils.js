238e92f3235e66c27f1073639cf96fa878e105cb
srj-17 <saugatrijal17@gmail.com>
2025-01-27 09:50:46 +0545

    Implement knights travils with BFS

--- knight-travils.js
+++ knight-travils.js
@@ -9,7 +9,7 @@ function hash(coordinate) {
 }
 
 // coordinates are of the form [horizontal, vertical]
-function createCoordinateTable() {
+const coordinates = (function createCoordinateTable() {
     let coordinateTable = [];
     for (let i = 0; i < SIZE; i++) {
         for (let j = 0; j < SIZE; j++) {
@@ -18,7 +18,24 @@ function createCoordinateTable() {
         }
     }
     return coordinateTable;
-}
+})();
+
+// create an adjacency list for the graph
+const graph = (function createGraph() {
+    let graph = [];
+    coordinates.forEach((coordinate, index) => {
+        graph[index] = getPossibleMoves(coordinate);
+    });
+
+    function getAdjacentVertices(vertex) {
+        return graph[vertex];
+    }
+
+    return {
+        graph,
+        getAdjacentVertices,
+    };
+})();
 
 function getPossibleMoves(coordinate) {
     let possibleMoves = [];
@@ -48,15 +65,45 @@ function getPossibleMoves(coordinate) {
     return possibleMoves;
 }
 
-// create an adjacency list for the graph
-function createGraph(coordinates) {
-    let graph = [];
-    coordinates.forEach((coordinate, index) => {
-        graph[index] = getPossibleMoves(coordinate);
-    });
-    return graph;
+// use BFS
+// but maintain a path of nodes to the destination instead of just node in the queue
+function findPath(source, destination, graph) {
+    let queue = [[source]];
+
+    // Necessary Because we can fall into a loop in Graph if we don't
+    let visited = new Set();
+    while (queue.length !== 0) {
+        const path = queue.shift();
+        const vertex = path.at(-1);
+
+        if (vertex === destination) return path;
+
+        if (!visited.has(vertex)) {
+            const adjacentVertices = graph.getAdjacentVertices(vertex);
+
+            // create new path for each neighbor and push that to the queue
+            for (let adjacent of adjacentVertices) {
+                let newPath = [].concat(path);
+                newPath.push(adjacent);
+                queue.push(newPath);
+            }
+
+            visited.add(vertex);
+        }
+    }
 }
 
-const coordinates = createCoordinateTable();
-const graph = createGraph(coordinates);
-console.log(graph[0]);
+function knightMoves(source, destination) {
+    source = hash(source);
+    destination = hash(destination);
+    const path = findPath(source, destination, graph);
+    console.log(`Congrats! You made it in ${path.length - 1} moves!`);
+    for (const coordinate of path) {
+        console.log(`(${coordinates[coordinate]})`);
+    }
+}
+
+console.log("For (3,3) to (4, 3)");
+knightMoves([3, 3], [4, 3]);
+console.log("For (0,0) to (3, 3)");
+knightMoves([0, 0], [3, 3]);
